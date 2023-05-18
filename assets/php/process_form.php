15db25585b5afa63ca4d6cd9b52db16fe80096d1
dkrmentor <dkrmentor@gmail.com>
2023-05-19 01:31:05 +0500

    commit3

---
+++ assets/php/process_form.php
@@ -0,0 +1,33 @@
+<?php
+if (isset($_POST['esubmit'])) {
+  // Retrieve form data
+  $name = $_POST['name'];
+  $address = $_POST['Locality'];
+  $phone = $_POST['phone'];
+  $date = $_POST['date'];
+  $state = $_POST['state'];
+  $zip = $_POST['Zip'];
+  $package = $_POST['package-select'];
+
+  // Build email message
+  $to = 'dkrmentor@gmail.com'; // Replace with your email address
+  $subject = 'New Appointment Booking';
+  $message = "Name: $name\n";
+  $message .= "Address: $address\n";
+  $message .= "Phone: $phone\n";
+  $message .= "Date: $date\n";
+  $message .= "State: $state\n";
+  $message .= "ZIP-Code: $zip\n";
+  $message .= "Package: $package\n";
+
+  // Send email
+  $headers = "From: $name <$to>";
+  if (mail($to, $subject, $message, $headers)) {
+    // Email sent successfully
+    echo "Thank you for booking an appointment. We will contact you shortly.";
+  } else {
+    // Error sending email
+    echo "Sorry, there was an error booking your appointment. Please try again later.";
+  }
+}
+?>
